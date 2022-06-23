928ad2ee8fca4696421cbb92cae3b90309a79b1b
Michael Bradvica <mjbradvica@protonmail.com>
2022-06-23 19:06:10 -0400

    fixed file, more config options

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,6 @@ import logo from "../logo.svg";
 import "./App.css";
 
 const App: React.FC = () => {
-  const hi = () => {
-    return 0;
-  };
-
   return (
     <div className="App">
       <header className="App-header">
