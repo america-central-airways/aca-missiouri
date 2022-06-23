65338bb3a475cbf689c6cd4470893b457c98c6a1
Michael Bradvica <mjbradvica@protonmail.com>
2022-06-23 19:16:23 -0400

    prettier config, sass, bulma, removed junk

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bulma/bulma.sass";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
